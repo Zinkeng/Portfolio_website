64b73640d94e1c07eca2ac9f05e0686d326f4166
Zinkeng <ajongafaczinkeng@gmail.com>
2024-05-03 16:47:42 +0100

    Updated

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["c:\Users\bawah\Desktop\Naroge_learn\Portfolio_website.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
